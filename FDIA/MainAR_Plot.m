971167a964f26bca0c551fcdeb3872fe8c95dd1a
luzp <luzp12@qq.com>
2019-03-30 17:47:09 +0800

    Create MainAR_Plot.m

---
+++ FDIA/MainAR_Plot.m
@@ -0,0 +1,18 @@
+AR_Sensor=7;
+figure(1)
+% for AR_Sensor=1:n-1
+    time=1:length(M(:,AR_Sensor));
+% subplot(4,2,AR_Sensor)
+plot(time,M(:,AR_Sensor),'-b',time,Mc(:,AR_Sensor),'-.r');
+
+xlim([30 k]);
+% end
+legend('M','Mc');
+
+figure(2)
+for f2=1:n
+subplot(4,2,f2)
+plot( (Mc(:,f2)-M(:,f2))/DataJudgement(2,f2));
+% legned('M','Mc');
+xlim([30 k]);
+end
