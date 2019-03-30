1e98b0c23a59c165c10565bbcae33a1accbec2f5
luzp <luzp12@qq.com>
2019-03-30 17:47:42 +0800

    Update dataplot.m

--- data/Tail_687_2/dataplot.m
+++ data/Tail_687_2/dataplot.m
@@ -14,18 +14,23 @@ for k = 1:1
         % B=zeros(100,N);
         %将目标变量提取到A矩阵
         A(:,1)=N1_1.data;
-        A(:,2)=FF_1.data./100;
-        A(:,3)=interp1(1:length(OIP_1.data),OIP_1.data,0.25:0.25:length(OIP_1.data),'linear');
-        A(:,4)=interp1(1:length(OIT_1.data),OIT_1.data,0.25:0.25:length(OIT_1.data),'linear');
-        A(:,5)=VIB_1.data.*100;
-        %A(:,6)=interp1(1:length(FIRE_1.data),FIRE_1.data,0.25:0.25:length(FIRE_1.data),'linear');
-        A(:,6)=MACH.data.*100;
-        A(:,7)=PLA_1.data./8;
-        A(:,8)=interp1(1:length(PT.data),PT.data,0.5:0.5:length(PT.data),'linear')./10;
-        A(:,9)=interp1(1:length(TAT.data),TAT.data,0.25:0.25:length(TAT.data),'linear');
-        
-end
+        A(:,2)=N2_1.data;
+        A(:,3)=FF_1.data;
+        A(:,4)=EGT_1.data;
+        A(:,5)=interp1(1:length(TAT.data),TAT.data,0.25:0.25:length(TAT.data),'linear');
+        A(:,6)=interp1(1:length(PT.data),PT.data,0.5:0.5:length(PT.data),'linear');
+        A(:,7)=interp1(1:length(OIP_1.data),OIP_1.data,0.25:0.25:length(OIP_1.data),'linear');
+        A(:,8)=interp1(1:length(OIT_1.data),OIT_1.data,0.25:0.25:length(OIT_1.data),'linear');
+        A(:,9)=VIB_1.data;
+        A(:,10)=PLA_1.data; 
+end 
 A=A(3000:10000,:);
-plot(A(:,[2 8 9]));
-legend('N1','FF','OIP','OIT','VIB','MACH','PLA','PT','TAT');
+A=A*diag(1./max(abs(A)));
+rankA=ones(length(A),1);
+ for i=1:length(A)-29
+     rankA(i,1)=rank(A(i:i+29,:),0.02);
+ end
+%  A=[A rankA];
+plot(rankA);
+% legend('N1','FF','OIP','OIT','VIB','MACH','PLA','PT','TAT','rank');
 acoor=corrcoef(A);
