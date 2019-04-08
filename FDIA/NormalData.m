14aeaec2a88cd80958bc3a39a280d88b57df89a3
luzp <luzp12@qq.com>
2019-04-08 14:45:42 +0800

    Update NormalData.m

--- FDIA/NormalData.m
+++ FDIA/NormalData.m
@@ -1,6 +1,7 @@
-function [NM,minM,maxM]=NormalData(M)
+function [NM,minM,maxM]=NormalData(M,D)
 [m,~]=size(M);
-minM=repmat(min(M),m,1);
-maxM=repmat(max(M),m,1)+0.1;
+minM=repmat(min(D),m,1);
+maxM=repmat(max(D),m,1)+0.1;
 NM=(M-minM)./(maxM-minM)+0.5;
+% NM=M-(minM+maxM)*0.5;
 
