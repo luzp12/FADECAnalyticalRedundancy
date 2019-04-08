f3dc2f699ca650bed1e3f40ff4733356dbfcba9c
luzp <luzp12@qq.com>
2019-04-08 14:45:36 +0800

    Update MainFDIA_Plot.m

--- FDIA/MainFDIA_Plot.m
+++ FDIA/MainFDIA_Plot.m
@@ -1,28 +1,25 @@
-time=1:10:floor(k/10)*10;
+time=1:10:floor(totaltime/10)*10;
 figure(1)
- for AR_Sensor=S_2hr1
+ for AR_Sensor=1:ntotal/2
     subplot(4,2,AR_Sensor)
-    plot(time,Data2Controller(time,AR_Sensor),'-black',time,Data2Sensor_Fault(time,S_2hr1(AR_Sensor)),'-r',time,Data2Sensor_Fault(time,S_2hr2(AR_Sensor)),'-b');
+    plot(time,Data2Controller(time,AR_Sensor),'-black',time,Data2Sensor_Plot(time,AR_Sensor),'-r',time,Data2Sensor_Plot(time,AR_Sensor+ntotal/2),'-b');
     legend('Output','A','B');
- end
- for AR_Sensor=S_1hr
-    subplot(4,2,AR_Sensor)
-    plot(time,Data2Controller(time,AR_Sensor),'-black',time,Data2Sensor_Fault(time,AR_Sensor),'-r',time,Data2Sensor_Normal(time,AR_Sensor),'-b');
-    legend('Output','A','Normal');
-    xlim([30 k]);
+%     xlim([1 k]);
  end
 
 figure(2)
-for AR_Sensor=1:n2hr+n1hr+npla+n2hr
+% for AR_Sensor=1:ntotal
     [xx,yy]=find(SensorFaultRecord==1);
 plot( xx,yy,'*r');
-xlim([30 k]);
-ylim([1  n2hr+n1hr+npla+n2hr]);
+xlim([1 k]);
+ylim([1  ntotal]);
 % title(num2str(AR_Sensor))
-end
+% end
 
-AR_Sensor=FaultMode(1,2);
+AR_Sensor=FaultMode(1,1);
 figure(3)
-plot(time,Data2Controller(time,AR_Sensor),'-black',time,Data2Sensor_Fault(time,AR_Sensor),'-r',time,Data2Sensor_Normal(time,AR_Sensor),'-b','MarkerSize',1);
+plot(time,Data2Controller(time,AR_Sensor),'-black',time,Data2Sensor_Plot(time,AR_Sensor),'-r',time,Data2Sensor_Normal(time,AR_Sensor),'-b','MarkerSize',1);
 legend('Output','Fault','Normal');
-ylim([DataJudgement(1,AR_Sensor) 1.5*DataJudgement(2,AR_Sensor)]);
+% ylim([DataJudgement(1,AR_Sensor) 1.1*DataJudgement(2,AR_Sensor)]);
+grid on;
+xlim([10*m k]);
